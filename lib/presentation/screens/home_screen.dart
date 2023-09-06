83c8a9260ff4f18897da17dbc3c5313139522ed1
Jorge Matos <jisapjisap@gmail.com>
2023-09-06 15:38:57 +0200

    Uso del Bloc

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -23,8 +23,13 @@ class HomeScreen extends StatelessWidget {
             subtitle: const Text('Gestor de estado compuesto'),
             trailing: const Icon(Icons.arrow_forward_rounded),
             onTap: () => context.push('/counter-bloc'),
-          )
+          ),
         
+          const Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Divider(),
+          ),
+
         ],
 
       ),
