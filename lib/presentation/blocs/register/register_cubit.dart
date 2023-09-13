42c87f23f8e084073fa15c0c33c908419d458fe0
Jorge Matos <jisapjisap@gmail.com>
2023-09-13 16:44:12 +0200

    input username

--- lib/presentation/blocs/register/register_cubit.dart
+++ lib/presentation/blocs/register/register_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:forms_app/infrastructure/inputs/inputs.dart';
+import 'package:formz/formz.dart';
 
 part 'register_state.dart';
 
@@ -11,9 +13,13 @@ class RegisterCubit extends Cubit<RegisterFormState> {  // Manejador del estado 
   }
 
   void usernameChanged(String value){ // Métodos que cambian el estado (su copia)
-    emit(
-      state.copyWith(
-        username: value
+
+    final username = Username.dirty(value); // Creamos una variable que recoge el value que cambio y lo asigna al campo Username que altera el state 
+    
+    emit(                                   // Se emite
+      state.copyWith(                       // una copia de la instancia del state
+        username: username,                 // con ese cambio de estate del username
+        isValid: Formz.validate([username]) // con la validación establecida en el imput de tipo Username
       )
     );
   }
--- lib/presentation/blocs/register/register_state.dart
+++ lib/presentation/blocs/register/register_state.dart
@@ -5,34 +5,38 @@ enum FormStatus { invalid, valid, validating, posting }
 class RegisterFormState extends Equatable { // Estado del formulario
 
 
-  final FormStatus formStatus; // Valores que se reciben
-  final String username;
+  final FormStatus formStatus; // Valores que se reciben según inputs definidos por FormZ
+  final bool isValid;
+  final Username username;
   final String email;
   final String password;
 
-  const RegisterFormState({   // Valores por defecto
+  const RegisterFormState({   // Valores por defecto 
     this.formStatus = FormStatus.invalid, 
-    this.username = '', 
+    this.isValid = false,
+    this.username = const Username.pure(), // Es un tipo de username sin modificar 
     this.email = '', 
     this.password = '',
   });
 
   RegisterFormState copyWith({ // Estado. Se trabaja con copias de la instancia del estado
     FormStatus? formStatus,
-    String? username,
+    bool? isValid,
+    Username? username,
     String? email,
     String? password,
 
   }) => RegisterFormState(
 
     formStatus: formStatus ?? this.formStatus, // Si existe el valor se aplica y sino el valor por defecto.
+    isValid: isValid ?? this.isValid,
     username: username ?? this.username,
     email: email ?? this.email,
     password: password ?? this.password
   );
 
   @override
-  List<Object> get props => [ formStatus, username, email, password ]; // Permite saber a equatable cuando el estado a cambiado en base a estas props
+  List<Object> get props => [ formStatus, isValid, username, email, password ]; // Permite saber a equatable cuando el estado a cambiado en base a estas props
 }
 
 
